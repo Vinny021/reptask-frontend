729eeca510452ddaa724bff4a345e9c78d6378f0
Vinny021 <viniciusbarbosa021@gmail.com>
2023-04-25 16:13:03 -0300

    Init Commit

---
+++ linux/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,15 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "/home/vinicius/snap/flutter/common/flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "/home/vinicius/Documents/UNIFEI/SIN412-DASI/reptask" PROJECT_DIR)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=/home/vinicius/snap/flutter/common/flutter"
+  "PROJECT_DIR=/home/vinicius/Documents/UNIFEI/SIN412-DASI/reptask"
+  "DART_DEFINES=RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ=="
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=/home/vinicius/Documents/UNIFEI/SIN412-DASI/reptask/.dart_tool/package_config.json"
+  "FLUTTER_TARGET=/home/vinicius/Documents/UNIFEI/SIN412-DASI/reptask/lib/main.dart"
+)
